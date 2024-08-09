81cc8fbd12c942aa2c73fe29bb0ffc3b9b1fbb40
Nate Morse <morse.nate@gmail.com>
2024-08-09 08:52:57 -0400

    Upload image too

--- script.js
+++ script.js
@@ -9,9 +9,53 @@ const scale = 5 // 10
 let gcodes = []
 
 const loadFile = (event) => {
-    console.log(event.target.files[0])
-    let imageEle = document.getElementById('output');
-    imageEle.src = URL.createObjectURL(event.target.files[0]);
+    const file = event.target.files[0];
+    const canvas = document.getElementById("uploadedCanvas")
+    const ctx = canvas.getContext("2d");
+    if (file) {
+        const img = new Image();
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            img.src = e.target.result;
+        }
+
+        img.onload = function() {
+            // canvas.width = img.width;
+            // canvas.height = img.height;
+            ctx.drawImage(img, 0, 0, w, h);
+            let uploadedImageData = ctx.getImageData(0, 0, w, h, { "willReadFrequently": true });
+            //console.log(uploadedImageData)
+            processImage(uploadedImageData)
+        }
+
+        reader.readAsDataURL(file);
+    }
+    // console.log(event.target.files[0])
+    // let imageEle = document.getElementById('uploadedImage');
+    // imageEle.src = URL.createObjectURL(event.target.files[0]);
+
+    // // const canvas = document.getElementById('canvasSnap');
+    // // const context = canvas.getContext('2d');
+    // // let capturedImageData = context.getImageData(0, 0, w, h, {"willReadFrequently":true});
+
+    // // Get canvas context
+    // const ctx = document.getElementById("uploadedCanvas").getContext("2d");
+
+    // // Load image
+    // const image = new Image();
+    // image.onload = () => {
+    //     // Draw the image into the canvas
+    //     ctx.drawImage(image, 0, 0);
+    // };
+    // image.src = event.target.files[0];
+
+
+
+    // const upLoadImageContext = imageEle.getContext('2d');
+    // let uploadedImageData = upLoadImageContext.getImageData(0, 0, w, h, { "willReadFrequently": true });
+    // console.log(uploadedImageData)
+    // processImage(uploadedImageData)
 }
 // const image = document.getElementById("output");
 
@@ -19,10 +63,9 @@ const loadFile = (event) => {
 let url = null
 let downFilename = "toBrush_"
 
-function triggerGenerateGCode()
-{
+function triggerGenerateGCode() {
     var text = gcodes.join('\n') // GCode text
-    var blob = new Blob([text], {type: "text/plain"})
+    var blob = new Blob([text], { type: "text/plain" })
     if (url != null) {
         URL.revokeObjectURL(url)
     }
@@ -65,42 +108,46 @@ ready.addEventListener("click", function () {
 // };
 
 const getColor = (img, x, y) => {
-    const i = x*4 + y*w*4
-    return [img.data[i], img.data[i+1], img.data[i+2], img.data[i+3]]
+    const i = x * 4 + y * w * 4
+    return [img.data[i], img.data[i + 1], img.data[i + 2], img.data[i + 3]]
 }
 
 const setColorAt = (img, c, x, y) => {
-    const i = x*4 + y*w*4
+    const i = x * 4 + y * w * 4
     img.data[i] = Math.min(255, Math.max(0, c[0]))
-    img.data[i+1] = Math.min(255, Math.max(0, c[1]))
-    img.data[i+2] = Math.min(255, Math.max(0, c[2]))
+    img.data[i + 1] = Math.min(255, Math.max(0, c[1]))
+    img.data[i + 2] = Math.min(255, Math.max(0, c[2]))
 }
 
 const lighterThan = (c1, c2) => (c1[0] >= c2[0] && c1[1] >= c2[1] && c1[2] >= c2[2])
 
 const scanFor = (c, image) => {
     const stroke = []
-    for(let x = 0; x < w; x++) {
-        for(let y = 0; y < h; y++) {
+    console.log("scanFor image", image)
+
+    for (let x = 0; x < w; x++) {
+        console.log("x", x)
+
+        for (let y = 0; y < h; y++) {
             const color = getColor(image, x, y)
             if (lighterThan(c, color)) {
-                stroke.push({x, y})
-                let y2 = y+1
-                while(y2 < h && lighterThan(c, getColor(image, x, y2))) {
-                    stroke.push({x, y:y2})
+                stroke.push({ x, y })
+                let y2 = y + 1
+                while (y2 < h && lighterThan(c, getColor(image, x, y2))) {
+                    stroke.push({ x, y: y2 })
                     y2++
                 }
-                return stroke
+                //return stroke
             }
         }
     }
-    return []
+    return stroke
 }
 
 const lightenColor = (c1, c2) => {
-    return [Math.min(255, c1[0]+(255-c2[0])), 
-    Math.min(255, c1[1]+(255-c2[1])), 
-    Math.min(255, c1[2]+(255-c2[2]))]
+    return [Math.min(255, c1[0] + (255 - c2[0])),
+    Math.min(255, c1[1] + (255 - c2[1])),
+    Math.min(255, c1[2] + (255 - c2[2]))]
 }
 
 const clearCanvas = (ctx, width, height) => {
@@ -111,44 +158,40 @@ const clearCanvas = (ctx, width, height) => {
     ctx.fill();
 }
 
-const r = (scale = 1, offset = 0) => Math.random()*scale+offset
+const r = (size = 1, offset = 0) => Math.random() * size + offset
 
 const addGCodes = (bs) => {
     const f = 4 * (scale / 10) // scale factor (150 / 24 === 6.25) (100 / 16 is 6.25) 
     const d = 2.0 // depth
     const o = 20 // x and y offset
     const zo = 1.5
-    const a = d*3 // approach
-    const ra = r()*f
-    gcodes.push(`G0 X${(bs.x*f+o+ra).toFixed(2)} Y${(bs.y*f-a+o).toFixed(3)} Z${zo.toFixed(2)}`)
-    gcodes.push(`G1 F1200 X${(bs.x*f+o+ra).toFixed(2)} Y${(bs.y*f+o).toFixed(2)} Z${(zo+d).toFixed(2)}`)
-    gcodes.push(`G1 X${(bs.x*f+o+ra).toFixed(2)} Y${(bs.y*f+bs.delta_y*f+o).toFixed(4)} Z${(zo+d).toFixed(2)}`)
-    gcodes.push(`G1 X${(bs.x*f+o+ra).toFixed(2)} Y${(bs.y*f+bs.delta_y*f+a+o).toFixed(2)} Z${zo.toFixed(2)}`)
+    const a = d * 3 // approach
+    const ra = r() * f
+    gcodes.push(`G0 X${(bs.x * f + o + ra).toFixed(2)} Y${(bs.y * f - a + o).toFixed(3)} Z${zo.toFixed(2)}`)
+    gcodes.push(`G1 F1200 X${(bs.x * f + o + ra).toFixed(2)} Y${(bs.y * f + o).toFixed(2)} Z${(zo + d).toFixed(2)}`)
+    gcodes.push(`G1 X${(bs.x * f + o + ra).toFixed(2)} Y${(bs.y * f + bs.delta_y * f + o).toFixed(4)} Z${(zo + d).toFixed(2)}`)
+    gcodes.push(`G1 X${(bs.x * f + o + ra).toFixed(2)} Y${(bs.y * f + bs.delta_y * f + a + o).toFixed(2)} Z${zo.toFixed(2)}`)
 }
 
-snap.addEventListener("click", function () {
+function processImage(imageData) {
     gcodes = []
-    const refCanvas = document.getElementById('canvasOutput');
-    const refContext = refCanvas.getContext('2d');
-    // clearCanvas(refContext)
-    // refContext.drawImage(referenceImage, 0, 0, w, h);
-    const refImageData = refContext.getImageData(0, 0, w, h);
+    // const refCanvas = document.getElementById('canvasOutput');
+    // const refContext = refCanvas.getContext('2d');
+    // // clearCanvas(refContext)
+    // // refContext.drawImage(referenceImage, 0, 0, w, h);
+    // const refImageData = refContext.getImageData(0, 0, w, h);
 
-    // clearCanvas(context)
-    context.drawImage(video, 0, 0, w, h);
-    const capturedImageData = context.getImageData(0, 0, w, h);
-    // console.log(getColor(capturedImageData, Math.floor(w/4), Math.floor(h/4)))
 
     const outCanvas2 = document.getElementById('canvasOutput2');
     const outContext2 = outCanvas2.getContext('2d');
     // clearCanvas(outContext2)
-
-    for(let x = 0; x < w; x++) {
-        for(let y = 0; y < h; y++) {
-            const color = getColor(capturedImageData, x, y)
+    console.log(w, h)
+    for (let x = 0; x < w; x++) {
+        for (let y = 0; y < h; y++) {
+            const color = getColor(imageData, x, y)
             outContext2.fillStyle = `rgb(${color[0]} ${color[1]} ${color[2]})`;
             outContext2.beginPath(); // Start a new path
-            outContext2.rect(x*scale, y*scale, scale, scale); // Add a rectangle to the current path
+            outContext2.rect(x * scale, y * scale, scale, scale); // Add a rectangle to the current path
             outContext2.fill(); // Render the path
         }
     }
@@ -162,53 +205,54 @@ snap.addEventListener("click", function () {
     clearCanvas(outContext3, 240, 160)
     clearCanvas(outContext4, 240, 160)
     // outContext4.globalCompositeOperation = "darken"
-    
+
     let paintColor = []
     let brushStrokes = []
-    
+
     // paintColor = [200, 200, 250] 
-    // brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushWithColor(paintColor, imageData, outContext3, outContext4);
     // paintColor = [250, 200, 200] 
-    // brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushWithColor(paintColor, imageData, outContext3, outContext4);
     // paintColor = [200, 250, 200] 
-    // brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushWithColor(paintColor, imageData, outContext3, outContext4);
 
     // paintColor = [140, 140, 140]
-    // brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4);
     // console.log(paintColor, brushStrokes)
 
+    paintColor = '#fa1243';
     paintColor = [255, 255, 70]
-    brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4, 0);
+    brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4, 0);
     // console.log(paintColor, brushStrokes)
     gcodes.push(`G0 Z0`, `G0 X0 Y0`, `M0`)
     brushStrokes.map(addGCodes)
 
     paintColor = [255, 255, 170]
-    brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4, 0);
+    brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4, 0);
     // console.log(paintColor, brushStrokes)
     gcodes.push(`G0 Z0`, `G0 X0 Y0`, `M0`)
     brushStrokes.map(addGCodes)
 
     paintColor = [255, 70, 255]
-    brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4, 1);
+    brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4, 1);
     // console.log(paintColor, brushStrokes)
     gcodes.push(`G0 Z0`, `G0 X0 Y0`, `M0`)
     brushStrokes.map(addGCodes)
 
     paintColor = [255, 170, 255]
-    brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4, 1);
+    brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4, 1);
     // console.log(paintColor, brushStrokes)
     gcodes.push(`G0 Z0`, `G0 X0 Y0`, `M0`)
     brushStrokes.map(addGCodes)
 
     paintColor = [70, 255, 255]
-    brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4, 2);
+    brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4, 2);
     // console.log(paintColor, brushStrokes)
     gcodes.push(`G0 Z0`, `G0 X0 Y0`, `M0`)
     brushStrokes.map(addGCodes)
 
     paintColor = [170, 255, 255]
-    brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4, 2);
+    brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4, 2);
     // console.log(paintColor, brushStrokes)
     gcodes.push(`G0 Z0`, `G0 X0 Y0`, `M0`)
     brushStrokes.map(addGCodes)
@@ -218,21 +262,29 @@ snap.addEventListener("click", function () {
     gcodes.push(`M2`)
 
     // paintColor = [255, 250, 250]
-    // brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4);
     // console.log(paintColor, brushStrokes)
     // paintColor = [255, 130, 255] 
-    // brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4);
     // console.log(paintColor, brushStrokes)
     // paintColor = [130, 255, 255] 
-    // brushStrokes = brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushStrokes = brushWithColor(paintColor, imageData, outContext3, outContext4);
     // console.log(paintColor, brushStrokes)
 
     // paintColor = [100, 100, 250] 
-    // brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushWithColor(paintColor, imageData, outContext3, outContext4);
     // paintColor = [250, 100, 100] 
-    // brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushWithColor(paintColor, imageData, outContext3, outContext4);
     // paintColor = [100, 250, 100] 
-    // brushWithColor(paintColor, capturedImageData, outContext3, outContext4);
+    // brushWithColor(paintColor, imageData, outContext3, outContext4);
+
+}
+
+snap.addEventListener("click", function () {
+    context.drawImage(video, 0, 0, w, h);
+    let capturedImageData = context.getImageData(0, 0, w, h, { "willReadFrequently": true });
+
+    processImage(capturedImageData)
 
 });
 
@@ -241,7 +293,7 @@ function brushWithColor(paintColor, capturedImageData, outContext3, outContext4,
     const refContext = refCanvas.getContext('2d');
     refContext.fillStyle = `rgb(${paintColor[0]} ${paintColor[1]} ${paintColor[2]} / 1)`;
     refContext.beginPath();
-    refContext.rect(ci*8, 0, 8, 8);
+    refContext.rect(ci * 8, 0, 8, 8);
     refContext.fill();
 
     let moreToDo = true
@@ -272,8 +324,9 @@ function brushWithColor(paintColor, capturedImageData, outContext3, outContext4,
         });
         moreToDo = (stroke.length > 0);
         if (moreToDo) {
-            allStrokes.push({x:stroke[0].x, y:stroke[0].y, delta_y:stroke.length})
+            allStrokes.push({ x: stroke[0].x, y: stroke[0].y, delta_y: stroke.length })
         }
+        moreToDo = false // trying just one pass 
     }
     return allStrokes;
 }
